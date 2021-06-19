a18d9534ff0990d3c61718fe9bea941cb3c24cd2
Swagat Suman <swagat.blue@gmail.com>
2021-06-19 09:13:38 +0530

    Google auth integration completed

--- constants/Environment.tsx
+++ constants/Environment.tsx
@@ -1,4 +1,4 @@
 export default {
-  IOS_CLIENT_ID: '237087913897-57qh7h4sooa0ink68agdg4n4bimha3ka.apps.googleusercontent.com',
-  ANDROID_CLIENT_ID: '237087913897-tmeepeal34822sdfrrsoqkmjrngn5i6a.apps.googleusercontent.com'
+  IOS_CLIENT_ID: '',
+  ANDROID_CLIENT_ID: ''
 }
